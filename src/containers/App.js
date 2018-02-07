faecdb3c628b11b5ade5061a7cb2a3a83d0b45ce
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-06 22:58:15 -0500

    connected video to the api and removed the stubbed data.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import '../App.css'
 import Videos from './Videos'
 
-const videos = [
-  {
-    title: "hello",
-    artist: "adele",
-
-  },
-  {
-    title: "goodbye",
-    artist: "beatles",
-
-  }
-]
-
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/videos')
+    .then(res => res.json())
+    .then(videos => this.setState({ videos }))
+  }
 
   render() {
+    console.log(this.state) // test test test
     return(
       <div className="App">
-        <Videos videos={videos} />
+        <Videos videos={this.state.videos} />
       </div>
     )
   }
