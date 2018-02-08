52658ca650617618341fb54328a034dbdecda4a9
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-08 13:56:01 -0500

    Converted Videos into a class.

--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import VideoCard from '../components/VideoCard'
 
-const Videos = (props) => (
-  <div>
-    <h1>Videos Component</h1>
-    {props.videos.map((video) => <VideoCard video={video} /> )}
+class Videos extends React.Component {
+  // constructor(props){
+  //   super(props)
+  //
+  //   this.state = {
+  //
+  //   }
+  // }
+  render() {
+    return(
+      <div>
+        <h1>Videos Card Container</h1>
+        {this.props.videos.map((video) => <VideoCard video={video} /> )}
 
 
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/WmzH0aKzifk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/WmzH0aKzifk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
-  </div>
-)
+      </div>
+    )
+  }
+}
+
+
 
 export default Videos
