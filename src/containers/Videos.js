1b0e0e482597c29e4ec5b42407a2d82417d6c353
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-07 22:42:05 -0500

    added a VideoCard component to hold the pressentation logic for the video cards.

--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import VideoCard from '../components/VideoCard'
 
 const Videos = (props) => (
   <div>
@@ -9,6 +10,7 @@ const Videos = (props) => (
         <p>artist: {video.artist}</p>
       </div>
     )}
+    <VideoCard />
   </div>
 )
 
