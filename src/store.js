5e5fc18c5c1ee0d62a80c67bcd3b6e1f3013df52
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-08 14:35:15 -0500

    added redux code to store.js and commented notes for build.

--- src/store.js
+++ src/store.js
@@ -5,7 +5,15 @@ import {
 } from 'redux'
 import thunk from 'redux-thunk'
 
+const videosReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'GET_VIDEOS_SUCCESS':
+      return 'hello world'
+    default: return state
+  }
+}
 
+// let videos = videosReducer(undefined, {type: "@@INIT"})
 
 // var store = {
 //   videos: [{...}],
