0081f03256a93c5fca5cc47dd5e4af694e7946a9
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-08 14:07:57 -0500

    Added a file for VideoForm. ToDo refactor for redux.

---
+++ src/containers/VideoForm.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class VideoForm extends React.Component {
+  render() {
+    return (
+      <div>
+      Add A Karaoke Video From Youtube
+        <form>
+
+        </form>
+      </div>
+    )
+  }
+}
+
+export default VideoForm
--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import VideoCard from '../components/VideoCard'
+import VideoForm from './VideoForm'
 
 class Videos extends React.Component {
   // constructor(props){
@@ -14,7 +15,7 @@ class Videos extends React.Component {
       <div>
         <h1>Videos Card Container</h1>
         {this.props.videos.map((video) => <VideoCard video={video} /> )}
-
+        <VideoForm />
 
           <iframe width="560" height="315" src="https://www.youtube.com/embed/WmzH0aKzifk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
