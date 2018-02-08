4d2d699213a62b1e3fcf481eb1afc7e0d86db5be
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-07 23:25:44 -0500

    hard coded an embedded video for the mulan song. TODO make this feature dynamic.

--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -5,6 +5,10 @@ const Videos = (props) => (
   <div>
     <h1>Videos Component</h1>
     {props.videos.map((video) => <VideoCard video={video} /> )}
+
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/WmzH0aKzifk" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
   </div>
 )
 
