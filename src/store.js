fa8fe1f183bffe92c506637b220b32d2b7975632
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-08 14:18:41 -0500

    Created a store.js file for redux code. added redux to project.

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import {
+  createStore,
+  applyMiddleware,
+  combineReducers
+} from 'redux'
+import thunk from 'redux-thunk'
+
+
+
+// var store = {
+//   videos: [{...}],
+//   videoFormData: [
+//     title: '',
+//     artist: '',
+//     youtube_url: '',
+//     img_url: ''
+//   ],
+//   currentVideos: [...]
+//   }
+// }
