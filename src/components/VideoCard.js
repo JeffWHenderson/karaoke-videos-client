bee7130b2a445f0d5b20665d3100b16c41597f67
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-07 22:54:01 -0500

    moved presentation from videos to video card.

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-const VideoCard = () => {
-  return `video card`
-}
+const VideoCard = ({ video }) => (
+  <div className="video-card" key={video.id}>
+    <h3>{video.title}</h3>
+    <p>artist: {video.artist}</p>
+  </div>
+)
 
 export default VideoCard
--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -4,13 +4,7 @@ import VideoCard from '../components/VideoCard'
 const Videos = (props) => (
   <div>
     <h1>Videos Component</h1>
-    {props.videos.map((video) =>
-      <div className="video-card" key={video.id}>
-        <h3>{video.title}</h3>
-        <p>artist: {video.artist}</p>
-      </div>
-    )}
-    <VideoCard />
+    {props.videos.map((video) => <VideoCard video={video} /> )}
   </div>
 )
 
