301bf32628e44caf82fff3a8e9f93c50dcf91c24
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-09 00:57:51 -0500

    added a reducer for video form data.

---
+++ src/reducers/videoFormData.js
@@ -0,0 +1,13 @@
+export default (state = {
+  title: '',
+  artist: '',
+  img_url: '',
+  youtube_url: ''
+}, action) => {
+    switch(action.type) => {
+      case 'UPDATED_DATA':
+        return action.videoFormData
+      default:
+        return state
+    }
+}
