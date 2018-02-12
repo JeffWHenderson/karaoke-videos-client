9228709a410bd5405cc1a40d374bd9570adae52c
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-11 23:20:43 -0500

    added react-router-dom to project. added links as stubs.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import '../App.css'
+import { Link, Route } from 'react-router-dom'
+
 import Videos from './Videos'
 import VideoForm from './VideoForm'
 import VideoPlayer from './VideoPlayer'
 import VideoQueue from './VideoQueue'
 
+
+
 class App extends React.Component {
 
   render() {
     return(
-      <div className="App">
-        <VideoForm />
-        <VideoPlayer />
-        <Videos />
-        <VideoQueue />
+      <div>
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/videos">All Videos</Link>
+        <Link to="/videos/new">Create Video</Link>
+        </nav>
+        <div className="App">
+          <VideoPlayer />
+          <VideoForm />
+          <Videos />
+          <VideoQueue />
+        </div>
       </div>
     )
   }
