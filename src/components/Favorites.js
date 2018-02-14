f8931542e00e4a5aaaf6228dd3332cccd1dfc8ec
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-02-13 21:52:02 -0500

    added Favorites container and component.

---
+++ src/components/Favorites.js
@@ -0,0 +1,5 @@
+const Favorites = (video = {title: 'stub', artist: 'test', youtube_url: 'ww=wk'}) => (
+  <div>
+    Favorites
+  </div>
+)
---
+++ src/containers/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Favorites from '../components/Favorites'
+
+class Favorites extends React.Component {
+  render() {
+    return {
+      <Favorites />
+    }
+  }
+}
+
+export default Favorites
